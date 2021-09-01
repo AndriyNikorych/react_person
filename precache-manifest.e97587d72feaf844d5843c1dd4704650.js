ec1c633f367b2d040ee4bbeee9557924307e35a6
Andriy Nikorych <andrei,nikorich@gmail.com>
2021-09-01 18:15:45 +0300

    Updates

---
+++ precache-manifest.e97587d72feaf844d5843c1dd4704650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246ebaa069282612b46c7d1f00d75f1d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a95b796b21bf22b31049",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a95b796b21bf22b31049",
+    "url": "./static/js/main.75a7d85c.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
